527d0a61798f8332819f02151788b4ae663ce8de
Mahmut Abdulhakim ARABACI <abdulhakimarabaci@gmail.com>
2022-07-28 17:22:34 +0300

    init

---
+++ HomelessWork1/hw1.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	fmt.Print("1 : Merhaba Dünya Yaz\n2 : Rasgele 2 sayının karesini al\n3 : Rasgele 2 sayıyı Topla\n4 : Rasgele 2 sayıyı Çarp\n5 : Rasgele 2 sayıyı Böl")
+	fmt.Println("Yapmak istediğiniz işlem numarasını giriniz....")
+	var Choice int
+	fmt.Scanln(&Choice)
+	rand.Seed(time.Now().UnixNano())
+	ChoiceAct(Choice)
+
+}
+
+func ChoiceAct(cha int) {
+	if cha < 1 || cha > 5 {
+		fmt.Println("Yanlış Giriş Yaptınız")
+	} else {
+		if cha == 1 {
+			Hw()
+		} else if cha == 2 {
+			fmt.Printf("Karesi : %d\n", sq(rand.Intn(100)))
+		} else if cha == 3 {
+			fmt.Printf("Toplamı %d\n", add(rand.Intn(200), rand.Intn(200)))
+		} else if cha == 4 {
+			fmt.Printf("Çarpımı %d\n", multiply(rand.Intn(100), rand.Intn(100)))
+		} else if cha == 5 {
+			fmt.Printf("Bölümü %.2f\n", newdivide(float64(rand.Intn(200)), float64(rand.Intn(200))))
+		}
+	}
+}
+
+func Hw() {
+	println("Merhaba Dünya")
+}
+func sq(x int) int {
+	fmt.Printf("Rasgele Sayınız %d\n", x)
+	return x * x
+}
+func add(x, y int) int {
+	fmt.Printf("1.Rasgele Sayınız %d\n", x)
+	fmt.Printf("2.Rasgele Sayınız %d\n", y)
+	return x + y
+}
+func multiply(x, y int) int {
+	fmt.Printf("1.Rasgele Sayınız %d\n", x)
+	fmt.Printf("2.Rasgele Sayınız %d\n", y)
+	return x * y
+}
+func newdivide(x, y float64) float64 {
+	x = math.Round((x * 100) / 100)
+	y = math.Round((y * 100) / 100)
+	fmt.Printf("1.Rasgele Sayınız %.0f\n", x)
+	fmt.Printf("2.Rasgele Sayınız %.0f\n", y)
+	if x > y {
+		return x / y
+	} else if y >= x {
+		return y / x
+	} else {
+		return -1
+	}
+}
